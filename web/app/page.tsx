f63ce45050dd9f9217d9cc9e910b0e831190ed9e
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2025-04-14 19:01:39 +0500

    Adds small shadcn-ui button and orange theme config

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { AllPosts } from "@/app/components/Posts";
 import GetStartedCode from "@/app/components/GetStartedCode";
+import { Button } from "@/components/ui/button";
 
 export default async function Page() {
   return (
@@ -16,6 +17,7 @@ export default async function Page() {
               <div className=" text-md leading-6 prose uppercase text-primary">
                 A starter template for
               </div>
+              <Button>ShadCN Button</Button>
               <h1 className="font-display text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter text-black">
                 <Link className="text-red-500 " href="https://sanity.io/">
                   Sanity
