84bc508b270b471d89102fb18603420f8ff1254d
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2025-04-25 23:43:47 +0500

    Adds basic design for the process

--- app/(with-layout)/layout.tsx
+++ app/(with-layout)/layout.tsx
@@ -5,8 +5,10 @@ import { MainSidebar } from '@/components/main-sidebar';
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-white flex overscroll-none overflow-hidden">
-      <MainSidebar />
-      {children}
+      <div>
+        <MainSidebar />
+      </div>
+      <div className="container flex items-center p-10">{children}</div>
     </div>
   );
 }
