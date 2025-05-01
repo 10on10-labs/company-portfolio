637bb3f2f7036e6ba714afb1e14e51e1429dc5ce
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2025-05-01 21:46:46 +0500

    Adds shadcn card, button and carousel for the services section

--- app/(with-layout)/layout.tsx
+++ app/(with-layout)/layout.tsx
@@ -6,7 +6,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <div className="bg-white flex overscroll-none overflow-hidden h-full w-full">
       <MainSidebar />
-      <div className="container">{children}</div>
+      <div className="container p-5 max-[600px]:pt-20">{children}</div>
     </div>
   );
 }
--- app/(with-layout)/services/service-card.tsx
+++ app/(with-layout)/services/service-card.tsx
@@ -1,26 +1,32 @@
-export type Service = {
-  name: string;
-  id: string;
-  categories: string[];
-};
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/shadcn/card';
+
+import { Service } from './services';
 
 export const ServiceCard = ({ service, step }: { service: Service; step: string | number }) => {
   return (
-    <div className="rounded-3xl md:w-1/3 pl-10 pr-10 pt-10 pb-10 border border-secondary shadow-sm">
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-2xl font-bold tracking-tight">{service?.name}</h2>
-        <div className="rounded-full border-2 border-secondary px-2.5 py-0.5 text-md font-bold">
-          {step}
-        </div>
-      </div>
-
-      <div className="space-y-1">
+    <Card className="rounded-3xl w-full  pl-5 pr-5 pt-10 pb-10 border border-secondary shadow-sm">
+      <CardHeader>
+        <CardTitle className="flex justify-between items-center mb-4">
+          <h2 className="text-2xl font-bold tracking-tight">{service?.name}</h2>
+          <div className="rounded-full border-2 border-secondary px-2.5 py-0.5 text-md font-bold">
+            {step}
+          </div>
+        </CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-1">
         {service?.categories?.map((category, index) => (
           <div key={index} className="border-b border-primary py-2">
             <p className="text-base font-medium">{category}</p>
           </div>
         ))}
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   );
 };
