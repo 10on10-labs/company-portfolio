b927324bb0b12433d8d85310c0e312daf5692854
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2025-05-21 01:48:10 +0500

    Adds small changes and homepage icon replacement

--- app/(with-layout)/layout.tsx
+++ app/(with-layout)/layout.tsx
@@ -6,8 +6,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <div className="flex flex-col items-center h-full w-full z-1">
       <TopNavbar />
-      <div className="w-full  h-full hide-scrollbar  mt-30 mb-20">{children}</div>
-      <footer>coming soon</footer>
+      <div className="w-full  h-full hide-scrollbar  mt-20 sm:mt-30 mb-20">{children}</div>
+      {/* <footer>coming soon</footer> */}
     </div>
   );
 }
