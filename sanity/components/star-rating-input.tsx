acd28816f13445da77b8face8e50c8ddf02f6e55
Osama Ehsan Qureshi <osamahsan@gmail.com>
2025-05-24 01:52:10 +0500

    removed redundant field declaration for star input

--- sanity/components/star-rating-input.tsx
+++ sanity/components/star-rating-input.tsx
@@ -1,7 +1,7 @@
 // ./components/StarRatingInput.jsx
 import { StarIcon } from '@sanity/icons';
 import { Button, Flex } from '@sanity/ui';
-import { FormField, NumberInputProps, NumberSchemaType, set, unset } from 'sanity';
+import { NumberInputProps, NumberSchemaType, set, unset } from 'sanity';
 
 export function StarRatingInput(props: NumberInputProps<NumberSchemaType>) {
   const { value, onChange, elementProps } = props;
@@ -11,26 +11,24 @@ export function StarRatingInput(props: NumberInputProps<NumberSchemaType>) {
   };
 
   return (
-    <FormField title={props.schemaType.title} description={props.schemaType.description}>
-      <Flex gap={2}>
-        {[1, 2, 3, 4, 5].map((star: number) => (
-          <Button
-            key={star}
-            mode="bleed"
-            padding={2}
-            onClick={() => handleStarClick(star)}
-            {...elementProps}
-          >
-            <StarIcon
-              style={{
-                color: value && value >= star ? '#ffc107' : '#e0e0e0',
-                width: '1.5em',
-                height: '1.5em',
-              }}
-            />
-          </Button>
-        ))}
-      </Flex>
-    </FormField>
+    <Flex gap={2}>
+      {[1, 2, 3, 4, 5].map((star: number) => (
+        <Button
+          key={star}
+          mode="bleed"
+          padding={2}
+          onClick={() => handleStarClick(star)}
+          {...elementProps}
+        >
+          <StarIcon
+            style={{
+              color: value && value >= star ? '#ffc107' : '#e0e0e0',
+              width: '1.5em',
+              height: '1.5em',
+            }}
+          />
+        </Button>
+      ))}
+    </Flex>
   );
 }
