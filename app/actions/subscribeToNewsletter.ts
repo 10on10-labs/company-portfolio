28529ccd1610e8d986c17283698fe87267bd4d5d
Osama Ehsan Qureshi <osamahsan@gmail.com>
2025-08-23 17:56:50 +0500

    Update subscribeToNewsletter.ts

--- app/actions/subscribeToNewsletter.ts
+++ app/actions/subscribeToNewsletter.ts
@@ -3,7 +3,7 @@
 export async function subscribeToNewsletter(formData: FormData) {
   const email = formData.get('email');
 
-  await fetch('localhost:3000/api/subscribe', {
+  await fetch('/api/subscribe', {
     method: 'POST',
     body: JSON.stringify({ email }),
   });
