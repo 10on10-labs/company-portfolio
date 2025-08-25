8c3fe5f91669af53c34acffb27ee65242d8f398b
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2025-08-25 16:08:29 +0500

    Feature-pricing-page: Adds pricing page

--- components/top-navbar.tsx
+++ components/top-navbar.tsx
@@ -10,7 +10,7 @@ import { AnimatePresence, motion, Variants } from 'motion/react';
 const menuItems = [
   { title: 'Home', href: '/', isSection: false },
   { title: 'Services', href: '/#services', isSection: true },
-  { title: 'Pricing', href: '/#pricing', isSection: true },
+  { title: 'Pricing', href: '/pricing', isSection: false },
   { title: 'Case Studies', href: '/#case-studies', isSection: true },
   { title: 'Process', href: '/#process', isSection: true },
   { title: 'About', href: '/about', isSection: false },
